dd4b6771513cc9140b33e91e63a9225fcec87d63
Mitya Selivanov <0x7fffff@guattari.ru>
2021-08-23 21:14:56 +0300

    [Tools] Update Visual Studio project files generation.

--- tools/gen-vs.py
+++ tools/gen-vs.py
@@ -1,6 +1,33 @@
 #!/usr/bin/python3
 
-import shutil, uuid
+import shutil, uuid, os, glob
+
+def get_sources(folder: str, prefix: str, ext: str):
+  files = list()
+  for f in glob.glob(os.path.join(folder, ext)):
+    files.append(
+      os.path.join(prefix, os.path.basename(f)))
+  for d in glob.glob(os.path.join(folder, '*', '')):
+    files += get_sources(
+      d, os.path.join(
+        prefix,
+        os.path.basename(os.path.dirname(d))
+      ), ext)
+  return files
+
+def gen_group(tag: str, files: list):
+  s = ''
+  for f in files:
+    s += '\n    <' + tag + ' Include="' + f + '" />'
+  if s:
+    s += '\n  ';
+  return s;
+
+headers_list = get_sources(os.path.join('..', 'source'), 'source', '*.h')
+sources_list = get_sources(os.path.join('..', 'source'), 'source', '*.cpp')
+
+includes = gen_group('ClInclude', headers_list)
+compiles = gen_group('ClCompile', sources_list)
 
 guid_sln_project = '{' + str(uuid.uuid1()).upper() + '}'
 guid_project     = '{' + str(uuid.uuid1()).upper() + '}'
@@ -12,10 +39,22 @@ def process(path: str):
   return content.replace(
     '{{GUID-SLN-PROJECT}}', guid_sln_project).replace(
     '{{GUID-PROJECT}}', guid_project).replace(
-    '{{GUID-SOLUTION}}', guid_solution)
+    '{{GUID-SOLUTION}}', guid_solution).replace(
+    '{{INCLUDE}}', includes).replace(
+    '{{COMPILE}}', compiles)
 
 def write_to(path: str, content: str):
+  dir = os.path.dirname(path)
+
+  if not os.path.exists(dir):
+    os.makedirs(dir)
+
   open(path, 'w').write(content)
 
-write_to('../laplace.sln', process('vs/laplace.sln.in'))
-write_to('../laplace.vcxproj', process('vs/laplace.vcxproj.in'))
+write_to(
+  os.path.join('..', 'laplace.sln'),
+  process(os.path.join('vs', 'laplace.sln.in')))
+
+write_to(
+  os.path.join('..', 'laplace.vcxproj'),
+  process(os.path.join('vs', 'laplace.vcxproj.in')))
