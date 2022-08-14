09f28c1506b8f1144517715caf7c8a7b740a1edc
Mitya Selivanov <0x7fffff@guattari.ru>
2022-08-14 06:11:22 +0400

    test

--- source/test/unittests/buffer.test.c
+++ source/test/unittests/buffer.test.c
@@ -271,7 +271,6 @@ TEST("buffer set value") {
   REQUIRE(BUFFER_GET(buf, h, 0, -1) == 0);
   BUFFER_DESTROY(buf);
 }
-*/
 
 TEST("buffer set value and adjust") {
   BUFFER_CREATE(buf, int64_t);
@@ -283,8 +282,8 @@ TEST("buffer set value and adjust") {
   for (int _ = 1; _;) BUFFER_ADJUST(_, buf);
   REQUIRE(BUFFER_GET(buf, h, 0, -1) == 42);
 }
+*/
 
-/*
 TEST("buffer set value, deallocate and get value") {
   BUFFER_CREATE(buf, int64_t);
   handle_t foo, bar;
@@ -298,6 +297,7 @@ TEST("buffer set value, deallocate and get value") {
   REQUIRE(BUFFER_GET(buf, bar, 0, -1) == 42);
 }
 
+/*
 TEST("buffer set value, deallocate, allocate and get value") {
   BUFFER_CREATE(buf, int64_t);
   handle_t h;
